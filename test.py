62a08d963c542bbff234374808956f02b044f347
sho watanabe <showatanabe0226@gmail.com>
2022-01-25 01:48:50 +0900

    洗濯指数を計算するための関数を追加

--- test.py
+++ test.py
@@ -1,19 +1,17 @@
 import requests
 from dotenv import load_dotenv
 import os
-import schemas.weather_api as schemas
 load_dotenv()
 
-
 def laundry_exponent(temp:str, humidity:str):
-    calc = round((0.81 * float(temp) + 0.01 * float(humidity) * (0.99 * float(temp) - 14.3) + 46.3), 3)
-    return calc
+    
+    cul = (0.81 * float(temp) + 0.01 * float(humidity) * (0.99 * float(temp) - 14.3) + 46.3)
+    return cul
 
-
-def get_forecast_data(city: schemas.City):
+def get_forecast_data(city: str):
     BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/"
     API_KEY = os.getenv("WEATHER_API_KEY")
-    url = (BASE_URL + f"?id={city.city_id}&lang=ja&units=metric&appid={API_KEY}")
+    url = (BASE_URL + f"?id={city}&lang=ja&units=metric&appid={API_KEY}")
     res = requests.get(url).json()
     forecast_data = {}
     for forecast in res["list"]:
@@ -22,7 +20,10 @@ def get_forecast_data(city: schemas.City):
         temp = forecast["main"]["temp"]
         humidity = forecast["main"]["humidity"] 
         wind = forecast["wind"]["speed"]
-        exponent = laundry_exponent(temp, humidity)
+        exponent = get_forecast_data(temp, humidity)
         forecast_data[f"{date}"] = {"天気": weather, "気温": temp, "湿度": humidity, "風速": wind, "洗濯指数": exponent} 
         
     return forecast_data
+
+
+
